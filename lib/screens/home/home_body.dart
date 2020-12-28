95f690bd75eca1f32811937bc7761575361ebce3
chuev <chuev@ntc.local>
2020-12-28 19:09:50 +0300

    new Pathfinder icons

--- lib/screens/home/home_body.dart
+++ lib/screens/home/home_body.dart
@@ -8,6 +8,7 @@ import 'package:orange_planet_app/screens/home/components/horizontal_card.dart';
 import 'package:orange_planet_app/screens/home/components/vertical_card.dart';
 import 'package:orange_planet_app/screens/stripes/stripesScreen.dart';
 import 'package:orange_planet_app/screens/trials/trialsScreen.dart';
+import 'package:orange_planet_app/size_config.dart';
 
 class HomeBody extends StatelessWidget {
 
@@ -26,8 +27,8 @@ class HomeBody extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Color(0xFFF1EFF1),
                 borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
                 )
               ),
               child: Padding(
@@ -41,47 +42,48 @@ class HomeBody extends StatelessWidget {
                       style: Theme.of(context).textTheme.headline5,
                     ),
                     SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        HorizontalCard(
-                          icon: Icons.book,
-                          title: "Законы и\nЗаповеди",
-                          press: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return Laws();
-                              }),
-                            );
-                          },
-                        ),
-                        HorizontalCard(
-                          icon: MdiIcons.cards,
-                          title: "\nНашивки",
-                          press: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return Stripes();
-                              }),
-                            );
-                          },
-                        ),
-                        HorizontalCard(
-                          icon: MdiIcons.guitarAcoustic,
-                          title: "\nИспытания",
-                          press: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return Trials();
-                              }),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
+                    Pathfinder(),
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //   children: <Widget>[
+                    //     HorizontalCard(
+                    //       icon: Icons.book,
+                    //       title: "Законы и\nЗаповеди",
+                    //       press: () {
+                    //         Navigator.push(
+                    //           context,
+                    //           MaterialPageRoute(builder: (context) {
+                    //             return Laws();
+                    //           }),
+                    //         );
+                    //       },
+                    //     ),
+                    //     HorizontalCard(
+                    //       icon: MdiIcons.cards,
+                    //       title: "\nНашивки",
+                    //       press: () {
+                    //         Navigator.push(
+                    //           context,
+                    //           MaterialPageRoute(builder: (context) {
+                    //             return Stripes();
+                    //           }),
+                    //         );
+                    //       },
+                    //     ),
+                    //     HorizontalCard(
+                    //       icon: MdiIcons.guitarAcoustic,
+                    //       title: "\nИспытания",
+                    //       press: () {
+                    //         Navigator.push(
+                    //           context,
+                    //           MaterialPageRoute(builder: (context) {
+                    //             return Trials();
+                    //           }),
+                    //         );
+                    //       },
+                    //     ),
+                    //   ],
+                    // ),
                     SizedBox(height: 25),
                     Text(
                       "Развлечения",
@@ -125,3 +127,70 @@ class HomeBody extends StatelessWidget {
   }
 }
 
+class Pathfinder extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> pathfinders = [
+      {"icon":"assets/icons/elka.png", "text": "Законы и заповеди"},
+      {"icon":"assets/icons/fire.png", "text": "Нашивки"},
+      {"icon":"assets/icons/palatka.png", "text": "Испытания"},
+    ];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ...List.generate(pathfinders.length, 
+          (index) => CategoryCard(
+            icon: pathfinders[index]["icon"],
+            text: pathfinders[index]["text"],
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return Trials();
+                }),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key key, this.icon, this.text, this.press,
+  }) : super(key: key);
+
+  final String icon, text;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        width: 80,
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(13),
+                child: Container(
+                  child: Image.asset(icon),
+                ),
+              )
+            ),
+            const SizedBox(height:5),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
