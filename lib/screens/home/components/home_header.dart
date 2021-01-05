c818bd4949eb083003e63a9bcef4b2483baa212f
Anton <misterchuis@gmail.com>
2021-01-05 11:49:34 +0300

    new icon for free version

--- lib/screens/home/components/home_header.dart
+++ lib/screens/home/components/home_header.dart
@@ -20,66 +20,62 @@ class HomeHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 40.0, left: 16, right: 8),
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Оранжевая планета",
-                style: Theme.of(context).textTheme.headline4.copyWith(
-                  color: Colors.orange,
+      padding: const EdgeInsets.only(top: 30.0, left: 8, right: 8),
+      child: Column(children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Text(
+            //   "Оранжевая планета",
+            //   style: Theme.of(context).textTheme.headline4.copyWith(
+            //     color: Colors.orange,
+            //   ),
+            // ),
+            PopupMenuButton<int>(
+              icon: SvgPicture.asset("assets/icons/menu.svg"),
+              onSelected: (value) {
+                if (value == 1) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FeedbackScreen()));
+                }
+                if (value == 2) {
+                  Navigator.push(context, _launchURL());
+                }
+              },
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text("Обратная связь"),
                 ),
-              ),
-              PopupMenuButton<int>(
-                icon: SvgPicture.asset("assets/icons/menu.svg"),
-                onSelected: (value) {
-                  if (value == 1) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => FeedbackScreen()));
-                  }
-                  if (value == 2) {
-                    Navigator.push(context, _launchURL());
-                  }
-                },
-                itemBuilder: (context) => [
-                  PopupMenuItem(
-                    value: 1,
-                    child: Text("Обратная связь"),
-                  ),
-                  PopupMenuItem(
-                    value: 2,
-                    child: Text("Оставить отзыв"),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Expanded(
-              child: Stack(
-                children: <Widget>[
-                  Image.asset(
-                    'icon/logoPrince.png',
-                    width: 210,
-                    //fit: BoxFit.fitWidth,
-                    //alignment: Alignment.top,
-                  )
-                ],
-              ),
+                PopupMenuItem(
+                  value: 2,
+                  child: Text("Оставить отзыв"),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Expanded(
+            child: Stack(
+              children: <Widget>[
+                Image.asset(
+                  'icon/logoPrince.png',
+                  width: 210,
+                  //fit: BoxFit.fitWidth,
+                  //alignment: Alignment.top,
+                )
+              ],
             ),
           ),
-        ]
-      ),
+        ),
+      ]),
     );
   }
 }
-
-
 
 // class HomeHeader extends StatelessWidget {
 //   const HomeHeader({
