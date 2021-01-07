c6c43b6c9bced3729f5a599f5aa9202d043afbfd
Anton <misterchuis@gmail.com>
2021-01-07 13:48:01 +0300

    version without popupmenuitem

--- lib/screens/home/components/home_header.dart
+++ lib/screens/home/components/home_header.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:orange_planet_app/screens/home/components/feedback.dart';
-import 'package:url_launcher/url_launcher.dart';
+//import 'package:url_launcher/url_launcher.dart';
 
 class HomeHeader extends StatelessWidget {
   const HomeHeader({
     Key key,
   }) : super(key: key);
 
-  _launchURL() async {
-    const url = 'https://flutter.dev';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
+  // _launchURL() async {
+  //   const url = 'https://flutter.dev';
+  //   if (await canLaunch(url)) {
+  //     await launch(url);
+  //   } else {
+  //     throw 'Could not launch $url';
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +25,6 @@ class HomeHeader extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            // Text(
-            //   "Оранжевая планета",
-            //   style: Theme.of(context).textTheme.headline4.copyWith(
-            //         color: Colors.orange,
-            //       ),
-            // ),
             PopupMenuButton<int>(
               icon: SvgPicture.asset("assets/icons/menu.svg"),
               onSelected: (value) {
@@ -40,19 +34,19 @@ class HomeHeader extends StatelessWidget {
                       MaterialPageRoute(
                           builder: (context) => FeedbackScreen()));
                 }
-                if (value == 2) {
-                  Navigator.push(context, _launchURL());
-                }
+                // if (value == 2) {
+                //   Navigator.push(context, _launchURL());
+                // }
               },
               itemBuilder: (context) => [
                 PopupMenuItem(
                   value: 1,
                   child: Text("Обратная связь"),
                 ),
-                PopupMenuItem(
-                  value: 2,
-                  child: Text("Оставить отзыв"),
-                ),
+                // PopupMenuItem(
+                //   value: 2,
+                //   child: Text("Оставить отзыв"),
+                // ),
               ],
             ),
           ],
